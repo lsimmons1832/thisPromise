76dd1faa1f6333b2f4843c96fbabf41c3b90b3b8
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-11 19:14:02 -0500

    save work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log("I'm linked");
+$(document).ready(function(){
+	$("#my-button").click(function(e){
+		console.log("event", e);
+		console.log("event.target", e.target);
+		console.log("this", $(this));
+		console.log("this", this);
+	});
+
+	
+});
